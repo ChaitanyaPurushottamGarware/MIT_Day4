4af3816d120cb1f0b31547bfb103cf3be899c17c
ChaitanyaPurushottamGarware <117964936+ChaitanyaPurushottamGarware@users.noreply.github.com>
2022-11-11 10:23:15 +0530

    Add files via upload

---
+++ address.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+float calculateSum(float num[])
+{
+}
+
+
+
+int main()
+{
+    float result.num[]={23.4,55,22.6,40.5,18};
+    result=calculateSum(num);
+    print("Result=%2f", result);
+    return 0;
+}
+
---
+++ disp.c
@@ -0,0 +1,3 @@
+#include<stdio.h>
+void displaynumb(int num[2][2]);
+
